ea3933d2ca61f12dfb98fca6eb5565632689c600
Kiran <nkirandroid@gmail.com>
2026-05-19 16:02:17 +0200

    rust practice

---
+++ rust/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    println!("Hello world");
+    println!("I'm Kiran !");
+
+    println!("I can print using  println!() and I need a semicolon at end!");
+
+    let nos = 1;
+
+    println!("I can print a number example - {}", nos);
+
+    print!("I can print on the same line using");
+    print!(" print()! \n");
+
+    println!("I can format the code using cargo fmt");
+
+    println!("I can also format it using rustfmt filename");
+
+    let sum = nos + 1;
+    println!("I can add nos using num1 + num2 = {}", sum)
+}
